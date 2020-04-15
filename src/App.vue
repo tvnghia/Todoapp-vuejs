337203b06e3aae72e74b55c5646758fe57616fba
JVghiaTran <nghia0969261137@gmail.com>
2020-04-15 21:07:20 +0700

    form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,169 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>File a Complaint</h1>
+          <hr>
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              v-model="dataUser.email">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model="dataUser.password">
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              class="form-control"
+              v-model="dataUser.age">
+          </div>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <label for="message">Message</label><br>
+            <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+          <textarea
+            id="message"
+            rows="5"
+            class="form-control"
+            v-model="message"></textarea>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <div class="form-group">
+            <label for="sendmail">
+              <input
+                type="checkbox"
+                id="sendmail"
+                value="SendMail"
+                v-model="sendMail"> Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input
+                type="checkbox"
+                id="sendInfomail"
+                value="SendInfoMail"
+                v-model="sendMail"> Send Infomail
+            </label>
+          </div>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <label for="male">
+            <input
+              type="radio"
+              id="male"
+              value="Male"
+              v-model="gender"> Male
+          </label>
+          <label for="female">
+            <input
+              type="radio"
+              id="female"
+              value="Female"
+              v-model="gender"> Female
+          </label>
+        </div>
+      </div>
+
+      <app-switch v-model="dataSwiched"></app-switch>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+          <label for="priority">Priority</label>
+          <select
+            id="priority"
+            class="form-control"
+            v-model="selectedPriority">
+            <option v-for="priority in priorities" :key="priority">{{ priority }}</option>
+          </select>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <button
+            class="btn btn-primary"
+            @click.prevent="submited">Submit!
+          </button>
+        </div>
+      </div>
+    </form>
+    <hr>
+
+    <div class="row" v-if="isSubmit">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Mail: {{ dataUser.email }}</p>
+            <p>Password: {{ dataUser.password }}</p>
+            <p>Age: {{ dataUser.age }}</p>
+            <p style="white-space: pre">Message: {{ message }}</p>
+            <p>{{ item }}<strong>Send Mail?</strong></p>
+            <ul>
+              <li v-for="item in sendMail" :key="item">{{ item }}</li>
+            </ul>
+            <p>Gender: {{ gender }}</p>
+            <p>Priority: {{ selectedPriority }}</p>
+            <p>Switched: {{ dataSwiched }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import appSwitch from './components/Switch'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      data () {
+        return {
+          dataUser: {
+            email: '',
+            password: '',
+            age: 25
+          },
+          message: 'Enter something msg',
+          sendMail: [],
+          gender: '',
+          selectedPriority: 'High',
+          priorities: ['High', 'Medium', 'Low'],
+          dataSwiched: true,
+          isSubmit: false
+        }
+      },
+      methods: {
+        submited() {
+          this.isSubmit = true
+        }
+      },
+      components: {
+        appSwitch
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
